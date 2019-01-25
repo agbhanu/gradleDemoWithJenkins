ea4bfe4b51b153a1158eeb023681ef3e67f6f300
bhanu_agrawal <bhanu_agrawal@persistent.com>
2019-01-25 14:53:45 +0530

    modified HelloWorld

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 
     public static void main(String args[]){
         System.out.println(new HelloWorld().sumOfTwoNum(2,4));
+        System.out.println("Hello World");
     }
 
     public int sumOfTwoNum(int a,int b){
